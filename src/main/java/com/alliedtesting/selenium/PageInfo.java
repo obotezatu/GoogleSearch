e2ac8b6325b36e58f835625a8fa14a0b40416c76
Oleg Botezatu <obotezatu@gmail.com>
2019-05-19 22:53:21 +0300

    1 commit

---
+++ src/main/java/com/alliedtesting/selenium/PageInfo.java
@@ -0,0 +1,27 @@
+package com.alliedtesting.selenium;
+
+public class PageInfo {
+	
+	private String url;
+	private String title;
+	private long occurrences;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public long getOccurrences() {
+		return occurrences;
+	}
+	public void setOccurrences(long occurrences) {
+		this.occurrences = occurrences;
+	}	
+}
